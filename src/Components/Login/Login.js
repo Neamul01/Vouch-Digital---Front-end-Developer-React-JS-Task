68dd680aec6d20ea9267ac75488a05a561573aa7
Noman01 <neamulhaque2002@gmail.com>
2022-07-30 00:43:12 +0600

    login form added

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,15 +1,83 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Card, Col, Layout, Row, Checkbox, Form, Input, Button } from 'antd';
+import './Login.css'
 
 const Login = () => {
-    const { Header, Footer, Sider, Content } = Layout;
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
     return (
-        <div>
+        <div className='login-page'>
             <Layout>
-                <Layout>
-                    <Sider>Sider</Sider>
-                    <Content>Content</Content>
-                </Layout>
+                <Row gutter={4}>
+                    <Col className='login-form-area' span={9}>
+                        <div className="login-title">
+                            <h2>Welcome Back</h2>
+                            <p>Good to see you again.</p>
+                        </div>
+                        <Form
+                            name="normal_login"
+                            className="login-form"
+                            initialValues={{
+                                remember: true,
+                            }}
+                            onFinish={onFinish}
+                        >
+                            <Form.Item
+                                name="username"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your Username!',
+                                    },
+                                ]}
+                            >
+                                <Input placeholder="Username" />
+                            </Form.Item>
+                            <Form.Item
+                                name="password"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your Password!',
+                                    },
+                                ]}
+                            >
+                                <Input
+
+                                    type="password"
+                                    placeholder="Password"
+                                />
+                            </Form.Item>
+                            <Form.Item>
+                                <Form.Item name="remember" valuePropName="checked" noStyle>
+                                    <Checkbox>Remember me</Checkbox>
+                                </Form.Item>
+
+                                <a className="login-form-forgot" href="">
+                                    Forgot password
+                                </a>
+                            </Form.Item>
+
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit" className="login-form-button">
+                                    Log in
+                                </Button>
+                                Or <a href="">register now!</a>
+                            </Form.Item>
+                        </Form>
+                    </Col>
+                    <Col span={15}>
+                        <div className='cover-div'>
+
+                        </div>
+                    </Col>
+                </Row>
             </Layout>
         </div>
     );
